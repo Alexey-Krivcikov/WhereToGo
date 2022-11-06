2a5e736da5062afbc9e638013ee17af9243f2a6e
Alexey Krivchikov <krivchikovalexei@yandex.ru>
2022-11-06 12:08:42 +0300

    feat: добавить в footer авторов

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 new WOW().init();
+
+let footer = document.querySelector('.footer');
+
+function addAuthors () {
+    footer.insertAdjacentHTML('beforeend', `
+<p class="footer__authors show">Бухбиндер Никита х Алексей Кривчиков</в>
+`);
+}
+
+footer.addEventListener('mouseover', addAuthors, {once: true});
